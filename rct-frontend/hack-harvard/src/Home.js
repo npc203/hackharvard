a594008bd7603c79e5f85490b0535d466e04c9e8
im-dagger <studyfreak1234@gmail.com>
2021-10-09 15:45:31 +0530

    minor frontend edits

--- rct-frontend/hack-harvard/src/Home.js
+++ rct-frontend/hack-harvard/src/Home.js
@@ -17,12 +17,12 @@ function Home() {
                 </h1>
                 <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
                 <div className="flex justify-center">
-                  <a href="create.html">
+                <Link to="/create"><a href="">
                     <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Create a Room</button>
-                  </a>
-                  <a href="join.html">
+                  </a></Link>
+                  <Link to="/join"><a href="join.html">
                     <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Join a Room</button>
-                  </a>
+                  </a></Link>
                 </div>
               </div>
               <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
