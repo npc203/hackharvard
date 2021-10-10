968de484fd4070226735de36bca03b9843d6c25c
npc203 <npc203@users.noreply.github.com>
2021-10-10 15:37:38 +0530

    saving progress

--- backend_py/controller_dect.py
+++ backend_py/controller_dect.py
@@ -1,14 +1,15 @@
 from pynput.keyboard import Key, Controller
 import cv2
 import mediapipe as mp
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-prevX = None
-prevY = None
-
+prevL = None
+prevR = None
+curr_pos = [0, 1, 0]
 direction = None
 keyboard = Controller()
 
@@ -18,8 +19,13 @@ def press(key):
     keyboard.release(key)
 
 
-# For webcam input:
+draw_line = False
 cap = cv2.VideoCapture(1 + cv2.CAP_DSHOW)
+if cap.isOpened():
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    draw_line = np.ones((height, width)) * 255
+
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         success, image = cap.read()
@@ -33,31 +39,51 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
         results = pose.process(image)
-        if results.pose_landmarks:
-            LS = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x
-            RS = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x
-            if prevX and prevY:
-                # print("LEFT",prevX - LS)
-                # print("RIGHT",prevY - RS)
-                if prevX - LS > 0 and direction != "A":
-                    direction = "A"
-                    press(Key.left)
-                if prevX - LS < 0 and direction != "B":
-                    direction = "B"
-                    press(Key.right)
-                print(direction)
-            else:
-                prevX = LS
-                prevY = RS
+
         # Draw the pose annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.pose_landmarks:
+            LS = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x
+            RS = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x
+            # print("LEFT", prevL, LS)
+            # print("RIGHT", prevR, RS)
+            if prevL and prevR:
+                if (prevL + prevR) / 2 - (LS + RS) / 2 > 0 and direction != "A":
+                    print("Moving Left")
+                    direction = "A"
+                    press(Key.left)
+                if (prevL + prevR) / 2 - (LS + RS) / 2 < 0 and direction != "B":
+                    print("Moving Right")
+                    direction = "B"
+                    press(Key.right)
+            else:
+                prevL = (LS + RS) / 2
+                prevR = (LS + RS) / 2
+            # print(direction, prevR, prevL)
+
+            # center_mass = [0, 0]
+            # avg_size = len(results.pose_landmarks.landmark)
+            # for marker in results.pose_landmarks.landmark:
+            #     center_mass[0] += marker.x or 0
+            #     center_mass[1] += marker.y or 0
+
+            # center_mass[0] = int(center_mass[0])
+            # center_mass[1] = int(center_mass[1])
+            # print(center_mass)
+            # cv2.circle(image, center_mass, 0, (0, 0, 255), 5)
+
         mp_drawing.draw_landmarks(
             image,
             results.pose_landmarks,
             mp_pose.POSE_CONNECTIONS,
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
         )
+        if draw_line.any():
+            coord_x = width // 3
+            cv2.line(image, (coord_x, 0), (coord_x, height), (0, 255, 0), thickness=1)
+            cv2.line(image, (coord_x * 2, 0), (coord_x * 2, height), (0, 255, 0), thickness=1)
         cv2.imshow("Pose detection", image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
